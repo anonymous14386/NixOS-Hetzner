792f8ea0c574761bd8ef729d45d055f411b467ed
Nick <ndiramio@gmail.com>
2026-01-03 13:04:56 -0500

    fixes for ssh key

--- configuration.nix
+++ configuration.nix
@@ -1,10 +1,11 @@
 { modulesPath, config, lib, pkgs, ... }: {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
-    (modulesPath + "/profiles/qemu-guest.nix")
     ./disk-config.nix
+    ./private.nix
   ];
 
+  # Bootloader
   boot.loader.grub = {
     enable = true;
     efiSupport = true;
@@ -12,33 +13,39 @@
     devices = [ "/dev/sda" "/dev/sdb" ];
   };
 
+  # SSH & Networking
   services.openssh = {
     enable = true;
     settings.PasswordAuthentication = false;
   };
+  
+  networking.hostName = "octopus-server";
 
-  # PASTE YOUR PUBLIC SSH KEY BELOW
-  users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDbr/7wNV/23NREyNjjzcC4XS1bHRktQD2DEGjFMn5GMarKbazUk+Lm0LUHdeKRLLcEIWZX3b+IvKkyVABz04queQ1ELD7fipWrYRdCjhW5WS2rNIKRrWUaGX05sXMC0V1l9VBHgNScuImT9PR5XECpdtcvIzLKwfFJIvd5EESH/E9gxefip1cAUmiFfKAb8e/YpW1I/WYoua160UpGXvWhK2dkGWcAQ1wY7T3dR7C6DQVtNR3Mowunl9mGoaXNqdNHiR+h7siSKldY+c8EF34run7pzPvqZpIRMHfjY/D6K5OSnGZc+lVV9/PmDO7TXm1dZPJjl1d8CWMNdciVlT0X9S+PhB/VEOorjkzg2pVzrYrLKLP/tsR/VH0MW28ZFUbBWM87nIXGzgML3LUOICnzFw1D2ALCFxSkxQ0UkhKzs8j5h5sfPb1fbdfXeQsVe8Kg5ryj3lVwM/6AC5JYP1qgYGsjOLThBlE8ID6Jm2FdgC4I7fn1WyOMIBQ5zn0NQT0/dEWqkm8GlUV4n5cOKlcK97+dA8h3OOdp0nw1FjDv0g0l4+ZXixs9JLGJuITHBKDDe+jyXDcMGlRVm8vegtMkRBhCu4y8xOy0c5JIYJmknG5Ko3q91gesdtSRzj8q8NrFNvWdXRnKQXm1fteMR7bpBlJue1KHcrkOb033wMZMww== JuiceSSH" 
+  # User Configuration
+  # Root gets keys from GitHub
+  users.users.root.openssh.authorizedKeys.keyFiles = [
+    (builtins.fetchurl "https://github.com/anonymous14386.keys")
   ];
 
-  # Enable nix-command and flakes
+  # Psychopathy user definition + keys from GitHub
+  users.users.psychopathy = {
+    isNormalUser = true;
+    description = "Psychopathy";
+    extraGroups = [ "networkmanager" "wheel" ]; 
+    packages = with pkgs; [];
+    openssh.authorizedKeys.keyFiles = [
+      (builtins.fetchurl "https://github.com/anonymous14386.keys")
+    ];
+  };
+
+  # System Settings
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # System packages
   environment.systemPackages = with pkgs; [
     git
     vim
     wget
   ];
 
-  users.users.psychopathy = {
-    isNormalUser = true;
-    description = "Psychopathy";
-    extraGroups = [ "networkmanager" "wheel" ]; # 'wheel' enables sudo
-    packages = with pkgs; [];
-  };
-
-  networking.hostName = "octopus-server";
-  system.stateVersion = "23.11"; 
+  system.stateVersion = "24.11";
 }
